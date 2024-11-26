13ca370d2e5fd619dd96bc050158d425f01b9a40
Will James <will.james@dbtlabs.com>
2024-11-26 21:32:59 +0000

    Rename a column

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -50,7 +50,8 @@ compute_booleans as (
         order_items_summary.count_drink_items,
         order_items_summary.count_order_items,
         order_items_summary.count_food_items > 0 as is_food_order,
-        order_items_summary.count_drink_items > 0 as is_drink_order
+        order_items_summary.count_drink_items > 0 as is_drink_order,
+        order_items_summary.order_cost as other_order_cost
 
     from orders
 
